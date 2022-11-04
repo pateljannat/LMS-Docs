0ac7e2b6325a0a4d23062cb2b47da55f8ac0698a
Jannat Patel <pateljannat2308@gmail.com>
2022-11-04 16:35:13 +0530

    docs: added config and sidebar

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vitepress';
+import sidebar from './sidebar';
+
+const tagline = 'Easy to Use, Open Source Learning Management System';
+
+export default defineConfig({
+  lang: 'en-US',
+  title: 'Frappe LMS',
+  description: tagline,
+  titleTemplate: tagline,
+  appearance: true,
+  lastUpdated: true,
+  outDir: "../dist",
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '../public/favicon.png' }],
+    ['link', { rel: 'stylesheet', href:'../public/style.css' }]
+  ],
+
+  themeConfig: {
+    logo: '../public/logo.png',
+    siteTitle: false,
+    nav: nav(),
+    sidebar: sidebar(),
+    editLink: {
+      pattern: 'https://github.com/pateljannat/lms_docs/edit/main/docs/:path',
+      text: 'Edit this page on GitHub',
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/frappe/lms' },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-present Frappe Technologies Pvt. Ltd.',
+    },
+  },
+});
+
+function nav() {
+  return [
+        {
+        text: 'Home',
+        link: 'https://frappelms.com/',
+        },
+        {
+            text: 'About Us',
+            link: 'https://frappelms.com/about-us',
+        },
+        {
+            text: 'Pricing',
+            link: 'https://frappelms.com/pricing',
+        }
+  ];
+}
